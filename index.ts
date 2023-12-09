21e67fca0e6e1bb3bed291ccdfc76c4aac9a84c8
Jocelyn <jocelyn@hotmail.com>
2023-12-09 22:06:32 +0700

    Init Express Server

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express, {Request, Response} from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Pink app listening on port ${port}`);
+});
