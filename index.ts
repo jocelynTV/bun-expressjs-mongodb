4903494a5a5525fec020a6bdb88ccc6bd5ae68a0
Jocelyn <jocelyn@hotmail.com>
2023-12-09 22:10:12 +0700

    Connect to DB

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 import express, {Request, Response} from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
+mongoose.connect("mongodb://127.0.0.1:27017/bun");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const userModel = mongoose.model("users", userSchema);
+
 app.use(express.json());
+
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hello World!");
